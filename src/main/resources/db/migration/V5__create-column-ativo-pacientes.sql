fd88951799f4f79470006b7b6a6d8547d5852341
Lucas <lucasp.oliveira822@gmail.com>
2025-07-29 08:20:06 -0300

    finalizing crud of pacientes

---
+++ src/main/resources/db/migration/V5__create-column-ativo-pacientes.sql
@@ -0,0 +1,4 @@
+alter table pacientes add column ativo tinyint;
+update pacientes set ativo = 1;
+alter table pacientes modify ativo tinyint not null;
+alter table pacientes modify ativo tinyint not null;
