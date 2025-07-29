cc5692ea05d9a9493df09e93aae0cbdfb5b4295a
Lucas <lucasp.oliveira822@gmail.com>
2025-07-29 11:16:44 -0300

    threat some errors

---
+++ src/main/java/med/voll/api/infra/TratadorErros.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratar404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratar400(MethodArgumentNotValidException ex){
+
+        var erros = ex.getFieldErrors().stream().map(DadosErroValidacao::new).toList();
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError erro){
+
+            this(erro.getField(),erro.getDefaultMessage());
+
+        }
+}
+}
